f7f199525a0499c34191cedb2f75063b6f2d37d8
Prince <prince.chattri@ez-xbrl.com>
2020-08-24 16:43:04 +0530

    bugfix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { ProductDetailPageComponent } from './product-detail-page/product-detail-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'plp', component: ProductPageComponent },
+  { path: 'pdp', component: ProductDetailPageComponent },
+  { path: '', redirectTo: '/plp', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailPageComponent } from './product-detail-page/product-detail-page.component';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductPageComponent,
+    ProductDetailPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
